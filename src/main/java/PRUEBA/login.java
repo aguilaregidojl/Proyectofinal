980ffec1ec8993238def3a072bca374b6ce408d3
java <java@DESKTOP-0AUP1R3>
2023-06-08 17:46:45 +0200

    Jueves 18h

---
+++ src/main/java/PRUEBA/login.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PRUEBA;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author java
+ */
+public class login {
+   public void validarusuario(JTextField usuario, JPasswordField contraseña){
+       try {
+           ResultSet rs=null;
+           PreparedStatement ps=null;
+           Modelo.conexion objetoconexion = new Modelo.conexion();
+           String consulta="SELECT * FROM Clientes WHERE Clientes.dni = (?) AND Clientes.telefono = (?);";
+           ps=objetoconexion.getConection().prepareStatement(consulta);
+           
+           //transformar password en variable string tipo cadena y llamamos la variable string
+           String contra= String.valueOf(contraseña.getPassword());
+         
+           ps.setString(1, usuario.getText());
+           ps.setString(2, contra);
+           
+           rs = ps.executeQuery();
+           if (rs.next()) {
+               System.out.println("El usuario es correcto");
+           }
+           else{
+               System.out.println("Usuario incorrecto vuelva a intentar");
+           }
+           
+           
+                   
+       } catch (Exception e) {
+           System.out.println("Error " +e.toString());
+       }
+       
+   }
+}
